4ab4cc3efb4b467ca5c484c27a2450952298eb36
Gabriel Lucas de Toledo Ribeiro <gabrieltr@users.noreply.github.com>
2016-11-01 14:04:58 -0200

    Adjusts for work with all users

--- AtualizaSOLMAN.user.js
+++ AtualizaSOLMAN.user.js
@@ -1,43 +1,43 @@
 // ==UserScript==
 // @name         Atualiza SOLMAN
 // @namespace    https://github.com/gabrieltr/AtualizaSolman/
-// @version      0.1.4
+// @version      0.1.6
 // @description  Atualizar dados do solman
 // @author       Gabriel Lucas de Toledo Ribeiro
 // @match        *solman.castgroup.com.br*/sap(*=)*
 // @homepageURL  https://openuserjs.org/scripts/gabrielgabrieltr/Atualiza_SOLMAN
 // @supportURL   https://openuserjs.org/scripts/gabrielgabrieltr/Atualiza_SOLMAN
-// @updateURL    https://github.com/gabrieltr/AtualizaSolman/
+// @updateURL    https://openuserjs.org/install/gabrielgabrieltr/Atualiza_SOLMAN.user.js
 // @icon         http://solman.castgroup.com.br:8000/sap/bc/bsp/sap/thtmlb_styles/sap_skins/nova/images/logo_sap.png
 // @grant        none
+// @require	    http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
 
     'use strict';
-    //console.debug("Atualizando a cada 20 segundos...");
+    //console.debug("Atualizando a cada 30 segundos...");
     var AtualizarContador=1;
+    var btn;
     function atualiza(){
         try{
-            var crmfrm = document.getElementById('CRMApplicationFrame');
-            //console.debug("CRMApplicationFrame="+crmfrm);
-            if(crmfrm!==null){
-                var workaf = crmfrm.contentDocument.getElementById("WorkAreaFrame1");
-                //console.debug("WorkAreaFrame1="+workaf);
-                if(workaf !== null){
-                    var btn = workaf.contentDocument.getElementById("C27_W89_V90_V92_Refresh");
-                    //console.debug("C27_W89_V90_V92_Refresh="+btn);
-                    if(btn !== null){
-                        btn.click();
-                        btn.text = "Atualizar: "+AtualizarContador++;  
-                        console.debug(Date.now() + " Atualizando... "+AtualizarContador);
-                    }
-                }
+            btn = $('img[alt="Atualizar"]');
+            //console.debug("C27_W89_V90_V92_Refresh="+btn);
+            if(btn !== null && btn.size()>=1){
+                btn.hide(250);
+                btn.click();
+                var texto = "Atualizando: " + AtualizarContador++;
+                console.debug(Date.now() + texto);
+                var linka=btn.parent().find('b');
+                linka.html(texto);
+                btn.show(400);
+            }else{
+                console.debug("botão atualizar nao encontrado neste frame..." + name);
             }
+            setTimeout(atualiza, 30000);
         }catch(e){
             console.debug("ERRO: "+e);
         }
-        setTimeout(atualiza, 20000);
     }
     atualiza();
 })();
